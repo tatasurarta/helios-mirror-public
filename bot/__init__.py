e9909ee2cbbeac23f9502fb23d977c8083da25eb
arshsisodiya <arshsisodiya@gmail.com>
2021-12-14 23:42:36 +0530

    Initialize Aria2c On Boot
    Co-authored-by: anasty17 <e.anastayyar@gmail.com>

--- bot/__init__.py
+++ bot/__init__.py
@@ -125,6 +125,24 @@ def get_client() -> qba.TorrentsAPIMixIn:
         logging.error(str(e))
         return None
 
+def aria2c_init():
+    try:
+        logging.info("Initializing Aria2c")
+        link = "https://releases.ubuntu.com/21.10/ubuntu-21.10-desktop-amd64.iso.torrent"
+        path = "/usr/src/app/"
+        aria2.add_uris([link], {'dir': path})
+        time.sleep(3)
+        downloads = aria2.get_downloads()
+        time.sleep(30)
+        for download in downloads:
+            aria2.remove([download], force=True, files=True)
+    except Exception as e:
+        logging.error(f"Aria2c initializing error: {e}")
+
+if not os.path.isfile(".restartmsg"):
+    threading.Thread(target=aria2c_init).start()
+    time.sleep(1)
+
 
 DOWNLOAD_DIR = None
 BOT_TOKEN = None
